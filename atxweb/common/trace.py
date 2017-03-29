48ee6105d5b011bf7b46e1f69b0436398c4aaaef
衣建中 <yjzn2154@corp.netease.com>
2017-03-29 14:24:24 +0800

    bug fix: add __init__.py

--- atxweb/common/trace.py
+++ atxweb/common/trace.py
@@ -5,13 +5,18 @@ import os
 import sys
 import argparse
 
+code_file = None
+
 def _trace(frame, event, arg_unused):
-    if os and os.path.basename(os.path.abspath(__file__)) == "trace.py":
+    global code_file
+    if os and os.path.basename(os.path.abspath(__file__)) == "trace.py" and os.path.basename(frame.f_code.co_filename) == code_file:
         print "$$lineno: %s" % (frame.f_lineno)
     return _trace
 
 
 def exec_file(filename):
+    global code_file
+    code_file = filename
     sys.settrace(_trace)
     exec open(filename) in {}
     sys.settrace(None)
